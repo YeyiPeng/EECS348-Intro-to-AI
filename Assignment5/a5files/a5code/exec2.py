3c34cefc65ddb2b810efa4948e2ced8ddb0fbd3e
YeyiPeng <frankpeng7@hotmail.com>
2016-06-12 16:11:45 -0700

    Remove unnecessary files

--- Assignment5/a5files/a5code/exec2.py
+++
@@ -1,5 +0,0 @@
-execfile("StrokeHmmbasic.py")
-x = StrokeLabeler()
-x.trainHMMDir("../trainingFiles/") #../ means go back a directory
-# x.confusion
-# x.labelFile("../trainingFiles/0128_1.6.1.labeled.xml", "resultstest111.txt")
